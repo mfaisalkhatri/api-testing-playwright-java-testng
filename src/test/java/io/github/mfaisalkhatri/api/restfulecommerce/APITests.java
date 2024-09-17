2533098e55101fe0130754d3383439c5e223addc
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2024-09-17 20:22:17 +0300

    added example for post api

--- src/test/java/io/github/mfaisalkhatri/api/restfulecommerce/APITests.java
+++ src/test/java/io/github/mfaisalkhatri/api/restfulecommerce/APITests.java
@@ -1,14 +1,59 @@
 package io.github.mfaisalkhatri.api.restfulecommerce;
 
 
+import com.microsoft.playwright.APIResponse;
+import com.microsoft.playwright.options.RequestOptions;
+import io.github.mfaisalkhatri.api.restfulecommerce.testdata.OrderData;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.github.mfaisalkhatri.api.restfulecommerce.testdata.OrderDataBuilder.getNewOrder;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
 
 public class APITests extends BaseTest{
 
+
+    private OrderData orderData;
+
+    @BeforeClass
+    public void setupTest() {
+        orderData = getNewOrder();
+    }
     @Test
     public void testShouldCreateNewOrders() {
-        //request.post("/addOrder").body()
+
+        int totalOrders = 4;
+        List<OrderData> orderList = new ArrayList<>();
+        for (int i = 0; i < totalOrders; i++) {
+            orderList.add(orderData);
+        }
+
+        APIResponse response = request.post("/addOrder", RequestOptions.create()
+                .setData(orderList));
+        assertEquals(response.status(), 201);
+
+        final JSONObject responseObject = new JSONObject(response.text());
+        final JSONArray ordersArray = responseObject.getJSONArray("orders");
+
+        assertEquals(responseObject.get("message"), "Orders added successfully!");
+        assertNotNull(ordersArray.getJSONObject(0).get("id"));
+        assertEquals(orderList.get(0).getUserId(), ordersArray.getJSONObject(0).get("user_id"));
+        assertEquals(orderList.get(0).getUserId(), ordersArray.getJSONObject(0).get("product_id"));
     }
 
+    @Test
+    public void testShouldGetAllOrders() {
 
+
+
+
+
+
+    }
 }
--- src/test/java/io/github/mfaisalkhatri/api/restfulecommerce/testdata/OrderDataBuilder.java
+++ src/test/java/io/github/mfaisalkhatri/api/restfulecommerce/testdata/OrderDataBuilder.java
@@ -1,6 +1,23 @@
 package io.github.mfaisalkhatri.api.restfulecommerce.testdata;
 
+import net.datafaker.Faker;
+
 public class OrderDataBuilder {
 
+    private static final Faker FAKER = new Faker();
 
+    public static OrderData getNewOrder() {
+        int randomInt = FAKER.number().numberBetween(1, 10);
+        int randomAmount = FAKER.number().numberBetween(400, 903);
+
+        return OrderData.builder()
+                .userId(String.valueOf(randomInt))
+                .productId(String.valueOf(randomInt))
+                .productName(FAKER.commerce().productName())
+                .productAmount(randomAmount).qty(randomInt)
+                .qty(randomInt)
+                .taxAmt(randomAmount)
+                .totalAmt(randomAmount)
+                .build();
+    }
 }
